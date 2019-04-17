1f7143fae51beb41b53295d9ad960fa42d9d6ad2
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-16 21:29:03 -0700

    final changes for deployment

--- src/helper-functions/onClick.js
+++ src/helper-functions/onClick.js
@@ -1,7 +1,7 @@
 export const nextPage = (e, page, setPage, topic) => {
   e.preventDefault();
   if (topic === "upcoming") {
-    page === 10 ? setPage(1) : setPage(page + 1);
+    page === 5 ? setPage(1) : setPage(page + 1);
   } else {
     page === 20 ? setPage(1) : setPage(page + 1);
   }
@@ -10,7 +10,7 @@ export const nextPage = (e, page, setPage, topic) => {
 export const prevPage = (e, page, setPage, topic) => {
   e.preventDefault();
   if (topic === "upcoming") {
-    page === 1 ? setPage(10) : setPage(page - 1);
+    page === 1 ? setPage(5) : setPage(page - 1);
   } else {
     page === 1 ? setPage(20) : setPage(page - 1);
   }
