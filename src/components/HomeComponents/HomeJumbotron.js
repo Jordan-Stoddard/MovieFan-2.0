8d9205c3d6f2860fbae16e50149e93ab46a0a126
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-19 15:49:55 -0700

    tested lazy loading library

--- src/components/HomeComponents/HomeJumbotron.js
+++ src/components/HomeComponents/HomeJumbotron.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Jumbotron } from "reactstrap";
 
 // Styles found in _homePageStyles
 // Direct parent component: Home
@@ -7,27 +6,29 @@ import { Jumbotron } from "reactstrap";
 const HomeJumbotron = props => {
   return (
     <div>
-      <Jumbotron className="home-jumbotron">
-            <div className="jumbo-text-container" data-aos="fade-right">
-              <h1 className="display-3">MovieFan</h1>
-              <br />
-              <p className="lead jumbotron-catchphrase">
-                Explore. Discover. Remember.
-              </p>
-              <br />
-              <p className="lead lead2">
-                MovieFan allows you to browse movies from top rated to upcoming,
-                from old favorites to the newest releases.
-              </p>
-            </div>
+      <div className="home-jumbotron">
+        <div className="jumbo-text-container" data-aos="fade-right">
+          <h1 className="display-3">MovieFan</h1>
+          <br />
+          <p className="lead jumbotron-catchphrase">
+            Explore. Discover. Remember.
+          </p>
+          <br />
+          <p className="lead lead2">
+            MovieFan allows you to browse movies from top rated to upcoming,
+            from old favorites to the newest releases.
+          </p>
+        </div>
 
-            <div className="jumbo-img" style={{backgroundImage: "url(https://image.tmdb.org/t/p/original/a1MlbLBk5Sy6YvMbSuKfwGlDVlb.jpg)"}} data-aos="fade-left">
-
-            </div>
-            
-         
-        
-      </Jumbotron>
+        <div
+          className="jumbo-img"
+          style={{
+            backgroundImage:
+              "url(https://image.tmdb.org/t/p/original/a1MlbLBk5Sy6YvMbSuKfwGlDVlb.jpg)"
+          }}
+          data-aos="fade-left"
+        />
+      </div>
     </div>
   );
 };
