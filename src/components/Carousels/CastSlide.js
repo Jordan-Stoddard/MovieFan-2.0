1ba1eb691b46fec45e1d0fb6e932c395bc63906b
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-18 22:30:12 -0700

    added descriptive comments

--- src/components/Carousels/CastSlide.js
+++ src/components/Carousels/CastSlide.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 
+// Renders the individual picture in the cast carousel
+
 export default function CastSlide({ cast } = this.props) {
   return (
     <div className="cast-img">
