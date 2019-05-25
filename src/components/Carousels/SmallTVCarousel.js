6c6e7d04a08eb54b92604de5815499e1c240022d
Jordan Stoddard <jjsincorporated@gmail.com>
2019-05-25 10:52:12 -0700

    Removed animations

--- src/components/Carousels/SmallTVCarousel.js
+++ src/components/Carousels/SmallTVCarousel.js
@@ -10,7 +10,7 @@ export default function SmallMovieCarousel({topic, genre, media}) {
   const TVShows = useGetTVTopic(topic)
   return (
     <div className="small-carousel">
-      <h2 className="small-carousel-genre" data-aos="fade-right">{genre}</h2>
+      <h2 className="small-carousel-genre">{genre}</h2>
       <Slider slidesToShow={setCarouselSlidesOnWidth(windowWidth())} infinite className="center" swipeToSlide>
       {TVShows.map(movie => 
         <MovieSlide movie={movie} key={movie.id} media={media}/>)}
